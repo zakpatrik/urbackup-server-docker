7769e548c55461a12a17207923d767654b059c81
themorlan <48086551+themorlan@users.noreply.github.com>
2019-12-06 19:46:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 cp -R /web-backup/* /usr/share/urbackup
 echo "/backups" > /var/urbackup/backupfolder
+usermod -u $PUID -o urbackup
+groupmod -g $PGID -o urbackup
 chown urbackup:urbackup /backups
 chown urbackup:urbackup /var/urbackup
 exec urbackupsrv "$@"
