f3a37763e0fb1681c081e93fcb3fbe2154620676
themorlan <48086551+themorlan@users.noreply.github.com>
2019-12-07 15:01:53 +0100

    Delete set_manifest.sh

--- set_manifest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION=${1:-latest}
-NAME=whatang/docker_urbackup
-export DOCKER_CLI_EXPERIMENTAL=enabled
-set -x
-docker manifest create --amend ${NAME}:${VERSION} ${NAME}:armhf-${VERSION} ${NAME}:amd64-${VERSION}
-docker manifest annotate ${NAME}:${VERSION} ${NAME}:armhf-${VERSION} --arch arm
-docker manifest push -p ${NAME}:${VERSION}
