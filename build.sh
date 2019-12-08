74c2407c2458eced3dfeac43311e730abefc3784
themorlan <48086551+themorlan@users.noreply.github.com>
2019-12-08 17:58:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@ set -x
 
 ARCH=${1:-amd64}
 VERSION=${2:-2.4.11}
-TAG_VERSION=${3:-${VERSION}}
 
 docker build \
               --build-arg ARCH=${ARCH} \
               --build-arg VERSION=${VERSION} \
               --build-arg IMAGE_ARCH=$([ "${ARCH}" == "armhf" ] && echo "arm32v7/debian:stretch" || ([ "${ARCH}" == "arm64" ] && echo "arm64v8/debian:stretch") || echo "debian:stretch") \
-              -t docker_urbackup:${ARCH}-${TAG_VERSION} \
+              -t docker_urbackup:${ARCH}-${VERSION} \
               .
