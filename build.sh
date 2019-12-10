1555e1ab51a31207ca427f610fde4dbf0d644142
Martin <martin@urbackup.org>
2019-12-10 21:43:11 +0100

    Use underscore to separate version and arch

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ VERSION=${2:-2.4.11}
 docker build \
               --build-arg ARCH=${ARCH} \
               --build-arg VERSION=${VERSION} \
-              --build-arg IMAGE_ARCH=$([ "${ARCH}" == "armhf" ] && echo "arm32v7/debian:stretch" || ([ "${ARCH}" == "arm64" ] && echo "arm64v8/debian:stretch") || echo "debian:stretch") \
-              -t docker_urbackup:${ARCH}-${VERSION} \
+              --build-arg IMAGE_ARCH=$([ "${ARCH}" == "armhf" ] && echo "arm32v7/debian:stretch" || ([ "${ARCH}" == "arm64" ] && echo "arm64v8/debian:stretch") || ([ "${BUILD_ARCH}" == "i386" ] && echo "i386/debian:stretch") || echo "debian:stretch") \
+              -t urbackup-server:${VERSION}_${ARCH} \
               .
