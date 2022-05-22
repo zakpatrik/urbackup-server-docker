1edf4d9d7481ef2063192e96e3c1b623169f911b
Jon <jonct@Shadow.local>
2022-05-22 14:20:53 -0400

    Convert tabs to spaces

--- build.sh
+++ build.sh
@@ -8,31 +8,31 @@ OPTS=${3}
 TAG=${VERSION}_${ARCH}
 
 case ${ARCH} in
-	"amd64")  IMAGE_ARCH="$BASE" ;;
-	"armhf")  IMAGE_ARCH="arm32v7/$BASE" ;;
-	"arm64")  IMAGE_ARCH="arm64v8/$BASE" ;;
-	"i386")   IMAGE_ARCH="i386/$BASE" ;;
-	*) echo "unrecognized architecture '$ARCH'" >>/dev/stderr ; exit 1 ;;
+    "amd64")  IMAGE_ARCH="$BASE" ;;
+    "armhf")  IMAGE_ARCH="arm32v7/$BASE" ;;
+    "arm64")  IMAGE_ARCH="arm64v8/$BASE" ;;
+    "i386")   IMAGE_ARCH="i386/$BASE" ;;
+    *) echo "unrecognized architecture '$ARCH'" >>/dev/stderr ; exit 1 ;;
 esac
 
 if [[ " ${*} " =~ " btrfs " ]]; then
-	BTRFS=1
-	TAG=${TAG}_btrfs
+    BTRFS=1
+    TAG=${TAG}_btrfs
 fi
 
 if [[ " ${*} " =~ " zfs " ]]; then
-	if [[ "$ARCH" != "amd64" ]]; then
-		echo "ZFS is only supported on amd64" ; exit 0
-	fi
-	ZFS=1
-	TAG=${TAG}_zfs
+    if [[ "$ARCH" != "amd64" ]]; then
+        echo "ZFS is only supported on amd64" ; exit 0
+    fi
+    ZFS=1
+    TAG=${TAG}_zfs
 fi
 
 docker build \
-		  --build-arg ARCH=${ARCH} \
-		  --build-arg VERSION=${VERSION} \
-		  --build-arg IMAGE_ARCH=${IMAGE_ARCH} \
-		  --build-arg BTRFS=${BTRFS} \
-		  --build-arg ZFS=${ZFS} \
-		  -t urbackup-server:${TAG} \
-		  .
+          --build-arg ARCH=${ARCH} \
+          --build-arg VERSION=${VERSION} \
+          --build-arg IMAGE_ARCH=${IMAGE_ARCH} \
+          --build-arg BTRFS=${BTRFS} \
+          --build-arg ZFS=${ZFS} \
+          -t urbackup-server:${TAG} \
+          .
